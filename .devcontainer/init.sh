ce48bf9fa142576703d05eb3edf90ebd5bc04218
ykmanufacturing <yoshiki.k.1991@gmail.com>
2025-04-05 22:09:34 +0900

    add devcontainer

---
+++ .devcontainer/init.sh
@@ -0,0 +1,6 @@
+cd /workspaces
+mkdir app
+cp /workspaces/zmk-config/config/west.yml /workspaces/app/
+west init -l app/
+ln -s /workspaces/zmk-config/.devcontainer/makefile /workspaces/zmk-config/
+echo 'build "make build" inside /workspaces/zmk-config.'

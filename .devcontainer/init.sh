893d95c1343763034b054c4185a6d526a47b8444
yoshiki <yoshiki.k.1991@gmail.com>
2025-04-07 15:57:34 +0000

    delete init.sh

--- .devcontainer/init.sh
+++
@@ -1,6 +0,0 @@
-cd /workspaces
-mkdir app
-cp /workspaces/zmk-config/config/west.yml /workspaces/app/
-west init -l app/
-ln -s /workspaces/zmk-config/.devcontainer/makefile /workspaces/zmk-config/makefile
-echo 'build "make build" inside /workspaces/zmk-config.'
